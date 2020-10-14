0e719b98ea505fd4a616e3bc05c8715661295ed3
Hoseok Lee <ihooi77@gmail.com>
2020-10-15 08:37:32 +0900

    Understanding about  component & Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+function Food ({ fav }){
+  return <h3>I like {fav} </h3>;
+}
+
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+       <Food fav="bap" />
     </div>
   );
 }
