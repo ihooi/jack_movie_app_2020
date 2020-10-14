73da0af713a664d5a5b0480b046268f25e680963
Hoseok Lee <ihooi77@gmail.com>
2020-10-15 08:40:12 +0900

    주석추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function Food ({ fav }){
-  return <h3>I like {fav} </h3>;
+function Food ({ fav }){    // function명은 대문자로 시작한다. 소괄호 안에 대괄호로 Props 정의
+  return <h3>I like {fav} </h3>;   //끝에 ; 잊지말자
 }
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-       <Food fav="bap" />
+       <Food fav="bap" />  //끝에 한칸띄고 슬래시 잊지말자
     </div>
   );
 }
