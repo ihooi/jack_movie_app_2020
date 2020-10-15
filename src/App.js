2b042ee68f42e91fb2626d2d0ec821c76f0c927d
Hoseok Lee <ihooi77@gmail.com>
2020-10-15 22:59:10 +0900

    04-1 비슷한 컴포넌트 여러개 만들기. map()을 쓰기 위해 리스트 생성

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ function Food( { fav }) {
 return <h3>I like { fav }</h3>;
 }
 
-// 13: 끝에 한칸띄고 슬래시 잊지말자
+const foodILike = [
+  {
+    name: 'kalgooksu',
+    image: 'https://www.chungjungone.com/knowhow/images/blog/ezr/ker42/1.jpg'
+  },
+  {
+    name: 'Kare',
+    image: 'https://img.daily.co.kr/@files/www.daily.co.kr/content/life/2015/20151216/3b75cf6dfbc03668f3fc9bb17a416b59.jpg'
+  },
+  {
+    name: 'bibimbap',
+    image: 'https://lh3.googleusercontent.com/proxy/bjHjex1nbUeZVLscBrJh4XGn_kUW8xoEzzpVWMkU5Vo-YDHXgexZo5bIi-e8mWljI7AwnSZ_1KPXeLcV859fxhhLCGGIH2lAuZY'
+  },
+];
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1> 
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="bap" />     
+      <h1>Hello</h1>     
     </div>
   );
 }
