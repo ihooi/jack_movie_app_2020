8548977eff41a9926e3db806157e62fc30fa5008
Hoseok Lee <ihooi77@gmail.com>
2020-10-15 22:39:16 +0900

    03-3 두 번째 리액트 기초개념: JSX 컴포넌트의 이해

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-function Food ({ fav }){    // function명은 대문자로 시작한다. 소괄호 안에 대괄호로 Props 정의
-  return <h3>I like {fav} </h3>;   //끝에 ; 잊지말자
+function Potato() {
+  return <h3>I like potato</h3>;
 }
 
+// 13: 끝에 한칸띄고 슬래시 잊지말자
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-       <Food fav="bap" />  //끝에 한칸띄고 슬래시 잊지말자
+      <h1>Hello</h1> 
+      <Potato />     
     </div>
   );
 }
