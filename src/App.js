ae434a5b7da7e76fabacdbec2fbd0b3eac1d0fec
Hoseok Lee <ihooi77@gmail.com>
2020-10-25 22:48:18 +0900

    05-3 클래스형 컴포넌트의 일생 알아보기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+// 생명주기 이해하기
+  constructor(props) {
+    super(props);
+    console.log('hello');
+  }
+
+  componentDidMount(){
+    console.log('component rendered');
+  }
+
+  componentDidUpdate(){
+    console.log('I just updated');
+  }
+
+  componentWillUnmount(){
+    console.log('Goodbye, cruel world');
+  }
+
 //state 정의
   state = {
     count: 0,
@@ -17,6 +35,7 @@ minus = () => {
 
 //렌더
   render() {
+    console.log('rendering')
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
