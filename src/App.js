1347700071d022952a2f4d00a028c67e4d151b80
Hoseok Lee <ihooi77@gmail.com>
2020-10-25 21:51:12 +0900

    04-3 map함수 적용하고, id, alt 적용

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ function Food( { name, picture }) {
   return (
     <div>
       <h2>I like {name} </h2>
-      <img src={picture} />
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: 'kalgooksu',
     image: 'https://www.chungjungone.com/knowhow/images/blog/ezr/ker42/1.jpg'
   },
   {
+    id: 2,
     name: 'Kare',
     image: 'https://img.daily.co.kr/@files/www.daily.co.kr/content/life/2015/20151216/3b75cf6dfbc03668f3fc9bb17a416b59.jpg'
   },
   {
+    id:3,
     name: 'bibimbap',
     image: 'https://lh3.googleusercontent.com/proxy/bjHjex1nbUeZVLscBrJh4XGn_kUW8xoEzzpVWMkU5Vo-YDHXgexZo5bIi-e8mWljI7AwnSZ_1KPXeLcV859fxhhLCGGIH2lAuZY'
   },
@@ -27,7 +30,7 @@ const foodILike = [
 function App() {
   return (
     <div>
-      {foodILike.map(x => (<Food name={x.name} picture={x.image}/>))}
+      {foodILike.map(x => (<Food key={x.id} name={x.name} picture={x.image}/>))}
     </div>
   );
 }
