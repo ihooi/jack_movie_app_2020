34ae067e6d2f2cd3b5d0f2064debeedf7669d8a4
Hoseok Lee <ihooi77@gmail.com>
2020-10-25 22:24:09 +0900

    05-1 state로 숫자 증감 기능 만들어보기

--- src/App.js
+++ src/App.js
@@ -1,48 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-function Food( { name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name} </h2>
-      <img src={picture} alt={name} />
-      <h3>{rating}/5</h3>
-    </div>
-  );
-}
+class App extends React.Component {
+//state 정의
+  state = {
+    count: 0,
+  }
 
-Food.prototype = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-}
+//숫자 늘리고 줄이는 함수
+add = () => {
+  console.log('add')
+};
 
-const foodILike = [
-  {
-    id: 1,
-    name: 'kalgooksu',
-    image: 'https://www.chungjungone.com/knowhow/images/blog/ezr/ker42/1.jpg',
-    rating: 4.5,
-  },
-  {
-    id: 2,
-    name: 'Kare',
-    image: 'https://img.daily.co.kr/@files/www.daily.co.kr/content/life/2015/20151216/3b75cf6dfbc03668f3fc9bb17a416b59.jpg',
-    rating: 5,
-  },
-  {
-    id:3,
-    name: 'bibimbap',
-    image: 'https://simplycook.imgix.net/recipes/Bibimbap-_720x480.jpg?dpr=1&fit=crop&fm=jpg&h=1000&ixlib=php-1.2.1&lossless=0&q=50&w=1200&s=621a9259e28cbbda23d38f524d6817ee',
-  },
-];
+minus = () => {
+  console.log('minus')
+};
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(x => (<Food key={x.id} name={x.name} picture={x.image} rating={x.rating} />))}
-    </div>
-  );
+//렌더
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
