d7234e5706c9856239707cd56253ccf205b7783f
Hoseok Lee <ihooi77@gmail.com>
2020-11-14 22:39:18 +0900

    07-1 영화 앱 전체 모습 수정하기

--- src/Movie.js
+++ src/Movie.js
@@ -4,12 +4,17 @@ import './Movie.css';
 
 function Movie ( { title, year, summary, poster, genres }) {
     return (
-        <div class="movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title}/>
-            <div class="movie__data">
-                <h3 class="movie__title"> {title} </h3>
-                <h5 class="movie__year"> {year} </h5>
-                <p class="movie__summary"> {summary} </p>
+            <div className="movie__data">
+                <h3 className="movie__title"> {title} </h3>
+                <h5 className="movie__year"> {year} </h5>
+                <ul className="movie__genres">
+                    {genres.map((genres, index) => {
+                        return <li key={index} className="movie__genres">{genres}</li>
+                    })}
+                </ul>
+                <p className="movie__summary"> {summary} </p>
             </div>
         </div>
     )
